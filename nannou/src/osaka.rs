cbb73dc2b3d1cba99e1c8d1ccd750f73f79e66f2
Shun2439 <shunta.kobayashi24@gmail.com>
2025-08-15 00:15:41 +0900

    chore: snapshot

--- nannou/src/osaka.rs
+++ nannou/src/osaka.rs
@@ -1,19 +1,19 @@
-use nannou::{image::imageops, prelude::*, rand::{rand, Rng}};
+use nannou::{prelude::*, rand::{rand, Rng}};
 
 /* https://scrapbox.io/prog-exercises/EXPO2025 */
 
 pub struct Spot {
-    pub deg: f64,
-    pub dx: f64,
-    pub dy: f64,
-    pub r: f64,
-    pub r2: f64,
-    pub vx: f64,
-    pub vy: f64,
+    pub deg: f32,
+    pub dx: f32,
+    pub dy: f32,
+    pub r: f32,
+    pub r2: f32,
+    pub vx: f32,
+    pub vy: f32,
 }
 
 impl Spot {
-    pub fn new(deg: f64, r: f64) -> Self {
+    pub fn new(deg: f32, r: f32) -> Self {
         let mut rng = rand::thread_rng();
 
         Spot {
@@ -28,19 +28,51 @@ impl Spot {
     }
 }
 
+pub fn draw_spot(app: &App, spot: &mut Spot) {
+    let width = app.window_rect().w();
+    let height = app.window_rect().h();
+
+    let ox = spot.deg.to_radians().cos() * width / 3.0;
+    let oy = spot.deg.to_radians().sin() * height / 3.0;
+
+    let draw = app.draw();
+
+    draw.ellipse()
+        .xy(vec2(ox + spot.dx, oy + spot.dy))
+        .wh(vec2(spot.r as f32, spot.r2 as f32))
+        .color(RED);
+}
+
+pub fn move_spot(spot: &mut Spot) {
+    let mut rng = rand::thread_rng();
+    // JavaScriptのロジックに合わせて、vxとvyを毎回計算しなおします。
+    // 乱数も浮動小数点数の範囲で生成します。
+    spot.vx = rng.gen_range(-40.0..=40.0) / 10.0 - spot.dx / 2.0;
+    spot.vy = rng.gen_range(-40.0..=40.0) / 10.0 - spot.dy / 2.0;
+    // 計算した速度で位置(dx, dy)を更新します。
+    spot.dx += spot.vx;
+    spot.dy += spot.vy;
+    spot.deg += 0.3; // 角度を更新
+}
+
 pub fn osaka(app: &App, frame: Frame) {
 
     let mut spots = Vec::new();
 
     for mut i in 0..360 {
         let r = rand::thread_rng().gen_range(30.0..=150.0);
-        spots.push(Spot::new(i as f64, r));
+        spots.push(Spot::new(i as f32, r));
         i += (r / 4.0) as i32;
     }
     // get canvas to draw on
     let draw = app.draw();
 
     draw.background().color(BLACK);
+
+    spots.iter_mut().for_each(|spot| {
+        draw_spot(app, spot);
+        move_spot(spot);
+    });
 
     // put everything on the frame
     draw.to_frame(app, &frame).unwrap();
